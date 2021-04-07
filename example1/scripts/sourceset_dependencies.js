65a5cac69e87b8e140603f4ded9a7f579cc7e4b7
twyatt <travis.i.wyatt@gmail.com>
2021-04-07 05:50:13 +0000

    Deploying to gh-pages from  @ ac9465e3cf4df1c38b419b21d2738c0806dd6e4d 🚀

---
+++ example1/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":example1:dokkaHtml/commonMain":[],":example1:dokkaHtml/jsMain":[":example1:dokkaHtml/commonMain"],":example1:dokkaHtml/jvmMain":[":example1:dokkaHtml/commonMain"]}'
---
+++ example2/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":example2:dokkaHtml/commonMain":[],":example2:dokkaHtml/jsMain":[":example2:dokkaHtml/commonMain"],":example2:dokkaHtml/jvmMain":[":example2:dokkaHtml/commonMain"]}'
